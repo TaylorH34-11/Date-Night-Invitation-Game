811136aac8f0ea7c2206908554737000cc24919d
TaylorH34-11 <thenning@ualberta.ca>
2026-06-10 09:34:06 -0600

    initial commit

---
+++ ds.js
@@ -0,0 +1,28 @@
+const noBtn = document.getElementById("noBtn");
+
+function moveButton() {
+    const padding = 20;
+
+    const maxX = window.innerWidth - noBtn.offsetWidth - padding;
+    const maxY = window.innerHeight - noBtn.offsetHeight - padding;
+
+    const x = Math.random() * maxX;
+    const y = Math.random() * maxY;
+
+    noBtn.style.left = `${x}px`;
+    noBtn.style.top = `${y}px`;
+}
+
+// Desktop
+noBtn.addEventListener("mouseenter", moveButton);
+
+// iPhone & Android
+noBtn.addEventListener("touchstart", (e) => {
+    e.preventDefault();
+    moveButton();
+});
+
+noBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+    moveButton();
+});
